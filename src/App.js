1e3d9a17bb9117bd2779ad9dd65518a965426e96
Aaron Janes <aaron.janes@gmail.com>
2023-02-19 10:32:12 -0700

    added tests

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import logo from './stackfails.jpg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World AAron
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Lets test this pipeline
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App" data-testid='background' style={{
+            backgroundImage: `url(${logo})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center"
+        }}>
+            <header className="App-header">
+                <p>
+                    Great job !
+                </p>
+                <a
+                    className="App-link"
+                    href="https://stackfails.io"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Check out the stack fails blog for more tutorials
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
+import logo from './stackfails.jpg'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders link to website', () => {
+    render(<App/>);
+    const linkElement = screen.getByText(/Check out the stack fails blog for more tutorials/i);
+    expect(linkElement).toBeInTheDocument();
 });
+
+// write a test to confirm background logo is present
+
+test('renders background logo', () => {
+    render(<App/>)
+//   find background image
+    const backgroundElement = screen.getByTestId('background')
+    expect(backgroundElement).toHaveStyle(`background-image: url(${logo})`)
+
+})
